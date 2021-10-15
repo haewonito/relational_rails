e767f70cf4a56dba0b3dc44c8388f5b1fbbf776c
Haewon Jeon <haewonito@gmail.com>
2021-10-14 20:42:06 -0600

    User Story 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/artists', to: 'artists#index'
   # get '/artists/:id', to: 'artists'
+  get '/artists/:id', to: 'artists#show'
+  # :id is a domain-specific language DSL
 end
--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'the artists show page' do
-    it 'displays the artist names' do
-      visit "/artists"
-    end
+  it 'user story 1: displays the artist names' do
+    artist = Artist.create(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
+    visit "/artists"
+  end
 
+#     As a visitor
+# When I visit '/parents/:id'
+# Then I see the parent with that id including the parent's attributes:
+# - data from each column that is on the parent table
+
+  it 'user story 2: displays the parent with id, with all its attributes' do
+    artist = Artist.create(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
+    visit "/artists/#{artist.id}"
+
+    expect(page).to have_content(artist.id)
+    expect(page).to have_content(artist.alive)
+    expect(page).to have_content(artist.name)
+    expect(page).to have_content(artist.first_name)
+    expect(page).to have_content(artist.age)
+    expect(page).to have_content(artist.created_at)
+
+  end
 end
