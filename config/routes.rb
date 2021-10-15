494d6b49d38f4accc81c58045bc2b4c8eccb28ba
Billy Frey <bfrey16@gmail.com>
2021-10-15 15:37:25 -0600

    fix: removed 's' at end of adk_mountains in table view as controller file instance variable does not have an 's'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
+
+
+
+  get '/adk_mountains', to: 'adk_mountains#names'
+  get '/adk_mountains/:id', to: 'adk_mountains#table'
 end
