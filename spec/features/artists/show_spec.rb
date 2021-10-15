b2059178ac453c28adf183667a7239e229b0cb47
Haewon Jeon <haewonito@gmail.com>
2021-10-14 20:49:49 -0600

    test: feature testing for /artists and /artists/:id

--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -1,15 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'the artists show page' do
-  it 'user story 1: displays the artist names' do
-    artist = Artist.create(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
-    visit "/artists"
-  end
-
-#     As a visitor
-# When I visit '/parents/:id'
-# Then I see the parent with that id including the parent's attributes:
-# - data from each column that is on the parent table
 
   it 'user story 2: displays the parent with id, with all its attributes' do
     artist = Artist.create(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
@@ -21,6 +12,5 @@ RSpec.describe 'the artists show page' do
     expect(page).to have_content(artist.first_name)
     expect(page).to have_content(artist.age)
     expect(page).to have_content(artist.created_at)
-
   end
 end
