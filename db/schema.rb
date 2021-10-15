fd60b4219874cf07510c968ab0e7168817a29125
Haewon Jeon <haewonito@gmail.com>
2021-10-15 15:16:21 -0600

    fix: child table - added title column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_221423) do
+ActiveRecord::Schema.define(version: 2021_10_15_210853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_10_14_221423) do
     t.bigint "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["artist_id"], name: "index_records_on_artist_id"
   end
 
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Artist do
-
+  it {should have_many :songs}
+  #do it with has_many commented out to prevent false positive
 end
