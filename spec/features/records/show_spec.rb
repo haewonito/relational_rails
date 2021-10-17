49008fcfadc10ea790884d6a6239397fa17dbaba
Haewon Jeon <haewonito@gmail.com>
2021-10-16 18:26:18 -0600

    test: feature tests up to US7

--- spec/features/records/show_spec.rb
+++ spec/features/records/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'as a visitor' do
-  describe 'when I visit the records index' do
+  describe 'when I visit the records show' do
 
     before(:each) do
       @artist = Artist.create!(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
@@ -9,14 +9,17 @@ RSpec.describe 'as a visitor' do
       @record2 = Record.create!(title: "Bob Dylan", genre: 'Folk', used: false, year: 1962, artist_id: @artist.id)
     end
 
-    it 'see all the records and their attributes' do
-      visit "/records"
+    it 'see all the attributes belonging to that record' do
+      visit "/records/#{@record1.id}"
 
       expect(page).to have_content(@record1.title)
       expect(page).to have_content(@record1.genre)
-      expect(page).to have_content(@record2.used)
-      expect(page).to have_content(@record2.year)
-      expect(page).to have_content(@record2.artist_id)
+      expect(page).to have_content(@record1.used)
+      expect(page).to have_content(@record1.year)
+      expect(page).to have_content(@record1.artist_id)
+      # expect(page).to_not have_content(@record2.artist_id)
+      # expect(page).to_not have_content(@record2.title)
+
     end
   end
 end
