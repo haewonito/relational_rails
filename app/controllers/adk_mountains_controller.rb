26197cd034c8e3523f951c2a5afe535904edc876
Billy Frey <bfrey16@gmail.com>
2021-10-17 18:49:37 -0600

    feat: complete user stories 6+7

--- app/controllers/adk_mountains_controller.rb
+++ app/controllers/adk_mountains_controller.rb
@@ -2,14 +2,15 @@ class AdkMountainsController < ApplicationController
   def index
   end
   def names
-    @adk_mountains = AdkMountain.all
+    @adk_mountains = AdkMountain.all.order(created_at: :desc)
   end
   def table
     @adk_mountain = AdkMountain.find(params[:id])
+    @count = @adk_mountain.hikers.length
   end
   def hikers_by_mountain
     @adk_mountain = AdkMountain.find(params[:id])
-    @hikers = @adk_mountain.hikers 
+    @hikers = @adk_mountain.hikers
 
   end
 end
