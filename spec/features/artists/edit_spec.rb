62936b74f4b9d069bee0179d5bdcefcb0801e98c
Haewon Jeon <haewonito@gmail.com>
2021-10-20 15:50:49 -0600

    test: US12

--- spec/features/artists/edit_spec.rb
+++ spec/features/artists/edit_spec.rb
@@ -4,27 +4,48 @@ require 'rails_helper'
 
 RSpec.describe 'this artist edit' do
 
-  it 'links to the edit page' do
-    artist = Artist.create!(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
+  describe "user story 12" do
+    it 'can edit artists' do
+      artist = Artist.create!(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
 
-    visit '/artists'
+      visit "/artists/#{artist.id}"
+      click_link "Update Artist"
 
-    click_button "Edit #{artist.name}"
+      expect(current_path).to eq("/artists/#{artist.id}/edit")
 
-    expect(current_path).to eq("/artists/#{artist.id}/edit")
+      fill_in 'artist[name]', with: 'Prince'
+      click_button 'Edit Artist'
+
+      expect(current_path).to eq("/artists/#{artist.id}")
+      expect(page).to have_content('Prince')
+      expect(page).to have_content('66')
+      expect(page).to_not have_content('McDowell')
+    end
   end
 
-  it 'can edit the artist' do
-    artist = Artist.create!(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
+  describe "user story 18" do
+    xit 'links to the edit page' do
+      artist = Artist.create!(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
 
-    visit "/artists/#{artist.id}/edit"
+      visit '/artists'
 
-    expect(page).to have_content('McDowell')
+      click_button "Edit #{artist.name}"
 
-    fill_in 'artist[name]', with: 'Prince'
-    click_button 'Update Artist'
+      expect(current_path).to eq("/artists/#{artist.id}/edit")
+    end
 
-    expect(current_path). to eq('/artists')
-    expect(page).to have_content('Prince')
+    xit 'can edit the artist' do
+      artist = Artist.create!(name: 'McDowell', first_name: 'Fred', age: 66, alive: false)
+
+      visit "/artists/#{artist.id}/edit"
+
+      expect(page).to have_content('McDowell')
+
+      fill_in 'artist[name]', with: 'Prince'
+      click_button 'Update Artist'
+
+      expect(current_path). to eq('/artists')
+      expect(page).to have_content('Prince')
+    end
   end
 end
